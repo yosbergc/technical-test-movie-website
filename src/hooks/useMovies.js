82e928be2e58613c75c51bad77584c99644b4d79
Yosber Guerrero <yosbergc@gmail.com>
2024-09-28 20:35:47 -0400

    added validation to prevent same query

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,23 +1,29 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 import { getMovies } from '../services/movies.js'
 import { debounce } from '../consts/debounce.js'
+
 function useMovies({ query }) {
     const [movies, setMovies] = useState(null)
     const [error, setError] = useState(false)
     const [loading, setLoading] = useState(false)
+    const queryRef = useRef(query)
     useEffect(() => {
         if (query === '') return
+        if (queryRef.current === query) return
         getQueryMovies(query)
     }, [query])
+
     const getQueryMovies = useCallback(debounce((query) => {
         setLoading(true)
         setError(false)
+        queryRef.current = query;
         getMovies(query)
             .then(data => setMovies(data))
             .catch(error => setError(error))
             .finally(setLoading(false))
-    }, 300), [])
+    }, 400), [])
+
     return {movies, error, loading}
 }
 
